9450fa7fa9f8c8e40d32421b39ed994526323919
Peter Marsh <peter_aaron_marsh@hotmail.com>
2025-04-02 12:46:53 +0900

    added scoring logic for different rolls - possibly buggy

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ interface DiceProps {
   //on dice click should lock the dice and highlight it
   onDiceClick: () => void;
   locked: boolean
+  lockedTurn?: number
 }
 
 interface ButtonProps {
@@ -24,9 +25,11 @@ function App() {
   const [lockRoll, setLockRoll] = useState(Array(6).fill(0));
   const [score, setScore] = useState(0);
   const [turnScore, setTurnScore] = useState(0);
+  const [rollScore, setRollScore] = useState(0);
   const [showTurnButton, setShowTurnButton] = useState(false);
   const [showRollButton, setShowRollButton] = useState(true);
   const [farkle, setFarkle] = useState(false);
+
 
   function rollDice(){
     var diceArray: Array<number> = [...dice]
@@ -37,27 +40,147 @@ function App() {
         newRoll.push(diceArray[i])
       }
     }
-    console.log("ROLL: " + diceArray)
+    //console.log("ROLL: " + diceArray)
 
     //make this into a farkle check function
     //this should also increment the turn score
     //end turn will then add the turn score to the total score
-    if((!newRoll.includes(1)) && (!newRoll.includes(5))){
-      setFarkle(true)
-    }
+    checkForFarkle(newRoll)
+
 
     //do spread operator and only copy over non locked dice
+    setTurnScore(turnScore + rollScore)
     setRoll(roll + 1)
     setDice(diceArray)
     setRollLockedDice(Array(6).fill(false))
 
     setShowRollButton(false)
     setShowTurnButton(false)
+    setRollScore(0)
     return 
   }
 
   function checkForFarkle(roll: Array<number>){
+    var rollscore = calculateRollScore(roll);
+    console.log("calculate roll score in check for farkle: " + rollscore)
+    //console.log("check for farkle: what is roll score: " + rollscore)
+    //console.log("turnscore - rollscore: " + (turnScore - rollscore))
+    if(rollscore == 0){
+      setFarkle(true)
+    }
+  }
 
+
+
+  function countOccurrences(arr:Array<any>, val:any){
+    return arr.reduce((a:any, v:any) => (v === val ? a + 1 : a), 0);
+  }
+  
+
+  function calculateRollScore(roll: Array<number>){
+    //console.log("calculating roll score, base points: " + turnScore)
+    var points = 0
+
+    //console.log("roll value before calculating dice per value: "+ roll)
+    var numOfDicePerValue: Array<number> = Array(6).fill(0)
+    //there must be a better way to do this...
+    roll.forEach(r => {
+      switch (r) {
+        case 1: 
+          numOfDicePerValue[0]++
+          break
+        case 2: 
+          numOfDicePerValue[1]++
+          break
+        case 3: 
+          numOfDicePerValue[2]++
+          break
+        case 4: 
+          numOfDicePerValue[3]++
+          break
+        case 5: 
+          numOfDicePerValue[4]++
+          break
+        case 6: 
+          numOfDicePerValue[5]++
+          break
+        default:
+          console.log("wtf")
+      }
+    })
+
+    //console.log("dice per value: " + numOfDicePerValue)
+
+    switch (true) {
+      case numOfDicePerValue.includes(6):
+        console.log("6 of a kind")
+        points+=3000
+        break
+      case numOfDicePerValue.includes(5):
+        console.log("5 of a kind")
+        points+=2000
+        if(numOfDicePerValue[0]==1) points+=100
+        if(numOfDicePerValue[4]==1) points+=50
+        break
+      case [2,4].every(i => numOfDicePerValue.includes(i)):
+        console.log("4 and a pair")
+        points+=1500
+        break
+      case countOccurrences(numOfDicePerValue, 2) == 3: 
+        console.log("3 doubles")
+        points+=1500
+        break
+      case numOfDicePerValue.includes(4):
+        console.log("4 of a kind")
+        points+=1000
+        points+=(numOfDicePerValue[0]*100)+(numOfDicePerValue[4]*50)
+        break
+      case countOccurrences(numOfDicePerValue, 3) == 2:
+        console.log("2 triples")
+        points+=2500
+        break
+      case numOfDicePerValue.toString() == [1,1,1,1,1,1].toString():
+        console.log("Straight")
+        points+=1500
+        break
+      case countOccurrences(numOfDicePerValue, 3) == 1:
+        //find what number the set of three is
+        var triple = numOfDicePerValue.indexOf(3) + 1
+        if(triple != 1){
+          console.log("Triple " + triple)
+          points+=(triple*100)
+          points+=((numOfDicePerValue[0])*100)+(numOfDicePerValue[4]*50)
+        } else {
+          console.log("Triple 1")
+          points+=300
+          points+=((numOfDicePerValue[0] - 3)*100)+(numOfDicePerValue[4]*50)
+        }
+
+        break
+      default:
+        console.log("1 or 5")
+        points+=(numOfDicePerValue[0]*100)+(numOfDicePerValue[4]*50)
+    }
+
+    //maybe a recursive function that
+
+    // Single 1 = 100
+    // Single 5 = 50
+    // Three 1s = 300
+    // Three 2s = 200
+    // Three 3s = 300
+    // Three 4s = 400
+    // Three 5s = 500
+    // Three 6s = 600 
+    // Four of any number = 1,000 X
+    // Five of any number = 2,000 X
+    // Six of any number = 3,000 X
+    // 1–6 straight = 1,500 X
+    // Three pairs = 1,500: do three pairs before 4 of a kind  X
+    // Four of any number with a pair = 1,500 X
+    // Two triplets = 2,500 X
+    
+    return points
   }
 
   function toggleDie(die: number){
@@ -75,10 +198,7 @@ function App() {
     if(!lockedDice[die] || (lockedDice[die] && lockRoll[die] == roll)){
 
       lockedDice[die] = !lockedDice[die]
-      console.log("before the thing")
       rollLockDice[die] = !rollLockDice[die]
-      console.log("lockedDice: " + lockedDice)
-      console.log("rollLockDice: " + rollLockDice)
 
       if(lockedDice[die]){
         lockedRoll[die] = roll
@@ -86,7 +206,23 @@ function App() {
         lockedRoll[die] = 0
       }
 
-      console.log("roll locked dice: " + rollLockDice)
+      //Get an array of how many dice have been rolled per dice value (1-6)
+      var diceArray: Array<number> = [...dice]
+      var scoringDice: Array<number> = []
+      for (let i = 0; i < 6; i++) {
+        if(rollLockDice[i]){
+          scoringDice.push(diceArray[i])
+        }
+      }
+
+      console.log("on toggle dice, scoring dice used to calculate score: " + scoringDice)
+      var rollScore = calculateRollScore(scoringDice)
+      console.log("on toggle dice, rollscore: " + rollScore)
+      console.log("roll score plus turn score: RS->" +  rollScore + " + TS=>" + turnScore)
+      
+      //rollScore+=turnScore
+      setRollScore(rollScore)
+
       if(rollLockDice.includes(true)) {
         setShowTurnButton(true)
         setShowRollButton(true)
@@ -94,10 +230,11 @@ function App() {
         setShowTurnButton(false)
         setShowRollButton(false)
       }
-
       setTurnLockedDice(lockedDice)
       setRollLockedDice(rollLockDice)
       setLockRoll(lockedRoll)
+
+
     } else {
       console.log("You're gonna carry that weight")
     }
@@ -108,15 +245,9 @@ function App() {
     setTurn(turn + 1)
 
     //This should be add points from turnPoints to total points
-    var points = 0
-    dice.forEach((d, index) => {
-      if(turnLockedDice[index]){
-        if(d == 1) points+=100
-        if(d == 5) points+=50
-      }
-    });
 
-    setScore(score + points)
+
+    setScore(score + turnScore + rollScore)
     setTurnLockedDice(Array(6).fill(false))
     setRollLockedDice(Array(6).fill(false))
     setRoll(0)
@@ -124,6 +255,8 @@ function App() {
 
     setShowRollButton(true)
     setShowTurnButton(false)
+    setTurnScore(0)
+    setRollScore(0)
     return
   }
 
@@ -136,16 +269,18 @@ function App() {
   return (
     <>
     <h1>Turn {turn}</h1>
-    <h2>Score {score}</h2>
+    <h2>TOTAL {score}</h2>
+    <h3>Roll Score {rollScore} + Turn Score {turnScore} = {turnScore + rollScore}</h3>
+    
     <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
 
       
-      <div><Dice value={dice[0]} onDiceClick={() => toggleDie(0)} locked={turnLockedDice[0]}/></div>
-      <div><Dice value={dice[1]} onDiceClick={() => toggleDie(1)} locked={turnLockedDice[1]}/></div>
-      <div><Dice value={dice[2]} onDiceClick={() => toggleDie(2)} locked={turnLockedDice[2]}/></div>
-      <div><Dice value={dice[3]} onDiceClick={() => toggleDie(3)} locked={turnLockedDice[3]}/></div>
-      <div><Dice value={dice[4]} onDiceClick={() => toggleDie(4)} locked={turnLockedDice[4]}/></div>
-      <div><Dice value={dice[5]} onDiceClick={() => toggleDie(5)} locked={turnLockedDice[5]}/></div>
+      <div><Dice value={dice[0]} onDiceClick={() => toggleDie(0)} locked={turnLockedDice[0]} lockedTurn={lockRoll[0]}/></div>
+      <div><Dice value={dice[1]} onDiceClick={() => toggleDie(1)} locked={turnLockedDice[1]} lockedTurn={lockRoll[1]}/></div>
+      <div><Dice value={dice[2]} onDiceClick={() => toggleDie(2)} locked={turnLockedDice[2]} lockedTurn={lockRoll[2]}/></div>
+      <div><Dice value={dice[3]} onDiceClick={() => toggleDie(3)} locked={turnLockedDice[3]} lockedTurn={lockRoll[3]}/></div>
+      <div><Dice value={dice[4]} onDiceClick={() => toggleDie(4)} locked={turnLockedDice[4]} lockedTurn={lockRoll[4]}/></div>
+      <div><Dice value={dice[5]} onDiceClick={() => toggleDie(5)} locked={turnLockedDice[5]} lockedTurn={lockRoll[5]}/></div>
     </div>
 
     
@@ -157,13 +292,44 @@ function App() {
   )
 }
 
+
+
 function Dice( props: DiceProps ) {
+  const rollOneLockColor = 'rgba(229, 22, 22, 0.64)'
+  const rollTwoLockColor = 'rgba(21, 55, 189, 0.64)'
+  const rollThreeLockColor = 'rgba(16, 168, 49, 0.64)'
+  const rollFourLockColor = 'rgba(255, 52, 204, 0.64)'
+  const rollFiveLockColor = 'rgba(188, 226, 16, 0.64)'
+
+  var lockColor = '';
+  switch(props.lockedTurn) {
+    case 1:
+      lockColor=rollOneLockColor
+      break
+    case 2:
+      lockColor=rollTwoLockColor
+      break
+    case 3:
+      lockColor=rollThreeLockColor
+      break
+    case 4:
+      lockColor=rollFourLockColor
+      break
+    case 5:
+      lockColor=rollFiveLockColor
+      break
+    default:
+      lockColor=rollOneLockColor
+  }
+  
+
   return <>
+
     <div 
 
     //<div style={{ visibility: this.state.driverDetails.firstName != undefined? 'visible': 'hidden'}}></div>
 
-      style={{border: props.locked?'5px solid rgba(229, 22, 22, 0.64)' : '3px solid rgba(0, 0, 0, 0.05)', padding: '15px', margin: '10px'}}
+      style={{border: props.locked?'5px solid '+lockColor : '3px solid rgba(0, 0, 0, 0.05)', padding: '15px', margin: '10px'}}
       onClick={props.onDiceClick}
       >
       {props.value}
